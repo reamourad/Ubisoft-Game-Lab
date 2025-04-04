80d2d0c3c2e5fb2ed2e1eb883497e33a54821b0c
Vishnu Rajendran <rajendran.vishnu97@gmail.com>
2025-04-04 00:04:09 -0400

    some fixes

--- Gamelabs2025/Assets/HighlightPlus/Runtime/Scripts/VRCheck.cs
+++ Gamelabs2025/Assets/HighlightPlus/Runtime/Scripts/VRCheck.cs
@@ -40,7 +40,7 @@ namespace HighlightPlus {
         static bool IsVrRunning() {
             bool vrIsRunning = false;
             displays.Clear();
-            SubsystemManager.GetInstances(displays);
+            SubsystemManager.GetSubsystems(displays);
             foreach (var displaySubsystem in displays) {
                 if (displaySubsystem.running) {
                     vrIsRunning = true;
